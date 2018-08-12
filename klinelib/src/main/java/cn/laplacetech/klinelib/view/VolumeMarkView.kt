861fb830c21537f490b0089e6249fb0df1f5894c
leefneg <leefengme@163.com>
2018-08-12 20:33:00 +0800

    准备添加成交量的文字描述

---
+++ klinelib/src/main/java/cn/laplacetech/klinelib/view/VolumeMarkView.kt
@@ -0,0 +1,12 @@
+package cn.laplacetech.klinelib.view
+
+import android.content.Context
+import cn.laplacetech.klinelib.R
+import com.github.mikephil.charting.components.MarkerView
+
+/**
+ * Created by limxing on 2018/8/12.
+ *
+ */
+class VolumeMarkView(context: Context?) : MarkerView(context, R.layout.markview_volume) {
+}
